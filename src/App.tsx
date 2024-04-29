6e3d672197f5e606ce8042ffb95d7d1795e8956a
j3r3my440 <j3r3my440@users.noreply.github.com>
2024-04-29 22:41:22 +0545

    feat: added vite svgr config

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,9 @@
 import "./App.css";
+import { RightIcon } from "./assets/svg";
 export default function App() {
-  return <div className="font-syne text-[35px] font-semibold">numenex</div>;
+  return (
+    <div className="font-syne text-[35px] font-semibold">
+      numenex <RightIcon />
+    </div>
+  );
 }
